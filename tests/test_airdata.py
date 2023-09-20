e6d69e69664f740eb459b24f1ee159ba8e59853b
LEEINWOO <49314154+develinu@users.noreply.github.com>
2023-09-21 02:34:51 +0900

    chore: 테스트 케이스 추가 및 의존성 파일 업데이트
    - airdata 전처리 TC 추가
    - pandas 의존성 추가

--- tests/test_airdata.py
+++ tests/test_airdata.py
@@ -2,6 +2,7 @@ import json
 from dotenv import load_dotenv
 
 from collect.airdata import request_airkorea_api, parse_airdata
+from preprocess.airdata import airdata_to_df, preprocessing_for_model_ver1, preprocessing_for_model_ver2
 
 
 load_dotenv()
@@ -13,7 +14,7 @@ def test_request_airkorea_api():
 
 
 def test_parse_airdata():
-    response = request_airkorea_api(station_name="서초구", page_no=1, data_term="DAILY")
+    response = request_airkorea_api(station_name="서초구", page_no=1, data_term="MONTH")
 
     if response.status_code != 200:
         return json.dumps(response)
@@ -21,3 +22,27 @@ def test_parse_airdata():
     airdata = parse_airdata(response.content)
 
     assert True == ("event_time" in airdata[-1].keys())
+
+
+def test_preprocessing_airdata_for_model_ver1():
+    response = request_airkorea_api(station_name="서초구", page_no=1, data_term="MONTH")
+
+    if response.status_code != 200:
+        return json.dumps(response)
+
+    airdata = parse_airdata(response.content)
+    df = airdata_to_df(airdata)
+    agg_df = preprocessing_for_model_ver1(df)
+    assert len(agg_df) > 20
+
+
+def test_preprocessing_airdata_for_model_ver2():
+    response = request_airkorea_api(station_name="서초구", page_no=1, data_term="MONTH")
+
+    if response.status_code != 200:
+        return json.dumps(response)
+
+    airdata = parse_airdata(response.content)
+    df = airdata_to_df(airdata)
+    agg_df = preprocessing_for_model_ver2(df)
+    assert len(agg_df) > 20
